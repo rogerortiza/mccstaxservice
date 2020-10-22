01d4f08068f08ae4fa9e472fb7b3fbd5804fb17a
rortiz <rortiz@contractroom.com>
2020-10-22 00:11:09 -0500

    new landing page

---
+++ application/views/home.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MCCS Tax Service</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<!-- <h1><a href="index.html">MCCS <span>Tax Service</span></a></h1> -->
+				<!-- <nav id="nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li>
+							<a href="#" class="submenu fa-angle-down">Page Layouts</a>
+							<ul>
+								<li><a href="left-sidebar.html">Left Sidebar</a></li>
+								<li><a href="right-sidebar.html">Right Sidebar</a></li>
+								<li><a href="no-sidebar.html">No Sidebar</a></li>
+								<li>
+									<a href="#">Submenu</a>
+									<ul>
+										<li><a href="#">Option One</a></li>
+										<li><a href="#">Option Two</a></li>
+										<li><a href="#">Option Three</a></li>
+										<li><a href="#">Option Four</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li><a href="elements.html">Elements</a></li>
+						<li><a href="#" class="button">Sign Up</a></li>
+					</ul>
+				</nav>-->
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<article>
+					<img src="assets/images/tax-man.jpg" alt="" />
+					<div class="inner">
+						<h2>MCCS Tax Service and Insurance</h2>
+					</div>
+				</article>
+				<article>
+					<img src="assets/images/tax-man2.jpg" alt="" />
+					<div class="inner">
+						<h2>MCCS Tax Service and Insurance</h2>
+					</div>
+				</article>
+				<article>
+					<img src="assets/images/tax2.jpg"  alt="" />
+					<div class="inner">
+						<h2>MCCS Tax Service and Insurance</h2>
+					</div>
+				</article>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper major-pad">
+				<div class="inner">
+					<section class="spotlight">
+						<div class="content">
+							<h2>Seguros de carro sin licencia</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id interdum magna, ultricies aliquet curabitur sed metus pellentesque, ornare sapien quis.</p>
+						</div>
+						<span class="image"><img src="assets/images/driver.jpg" alt="" /></span>
+					</section>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper content-pad">
+				<div class="inner">
+					<div class="features">
+						<section class="content">
+							<span class="icon-wrapper major"><span class="icon solid fa-pencil-alt"></span></span>
+							<h3>Servicio de Notario</h3>
+							<p>Proin consequat luctus elit, nec blandit tellus ut volutpat magna. mi euismod elementum lectus et consequat etiam lorem etiam sed tempus. Feugiat veroeros lorem ipsum dolor.</p>
+						</section>
+						<section class="content">
+							<span class="icon-wrapper major"><span class="icon solid fa-paper-plane"></span></span>
+							<h3>Preparacion de impuestos</h3>
+							<p>Pellentesque, ornare sapien quis, tristique ante. Proin nec facilisis odio. Integer elementum nunc nec leo interdum, non tristique eros laoreet. Integer vitae erat suscipit commodo.</p>
+						</section>
+						<section class="content">
+							<span class="icon-wrapper major"><span class="icon solid fa-list-ol"></span></span>
+							<h3>Aplicacion y renovacion de numeros ITIN</h3>
+							<p>Duis vulputate sit amet metus quis facilisis. Sed dapibus neque erat fringilla tincidunt. Nullam sapien et sapien, iaculis ac varius ultrices nec metus. Aenean ultricies magna.</p>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<h2>Nuestro Team</h2>
+					<div class="posts">
+						<section class="post">
+							<span class="image"><img src="assets/images/pic01.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Pablo Ruano</h3>
+								<p>Aenean ultricies magna non sapien rhoncus, ac ullamcorper lorem convallis. Quisque at venenatis nisi, amet finibus mauris. Sed sodales ultricies eros, sit amet sodales sapien.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">More</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="post">
+							<span class="image"><img src="assets/images/pic01.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Gema</h3>
+								<p>Ultrices nec metus. Aenean ultricies magna et sapien rhoncus ac ullamcorper lorem convallis. Quisque at venenatis nisi amet finibus mauris. Sed sodales ultricies magna etiam.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">More</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="post">
+							<span class="image"><img src="assets/images/pic01.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Cesar</h3>
+								<p>Quis interdum. Lorem quis lacus justo. Sed libero condimentum vehicula sem vel, mattis amet mauris. Nullam lacinia sit amet felis vel vestibulum. Morbi aliquam aenean.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">More</a></li>
+								</ul>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- Contact -->
+			<section id="contact" class="wrapper split">
+				<div class="inner">
+					<section>
+						<h2>Envianos un mensaje</h2>
+						<form method="post" action="#">
+							<div class="row gtr-uniform">
+								<div class="col-6 col-12-large col-6-medium col-12-xsmall">
+									<label for="name">Nombre</label>
+									<input type="text" name="name" id="name" required/>
+								</div>
+								<div class="col-6 col-12-large col-6-medium col-12-xsmall">
+									<label for="email">Email</label>
+									<input type="email" name="email" id="email" required />
+								</div>
+								<div class="col-12">
+									<label for="tipo-servicio">Tipo de Servicio</label>
+									<select name="tipo-servicio" id="tipo-servicio" rows="5" required>
+										<option value="" disabled selected>Selecciona una opcion</option>
+										<option value="1">Seguro de carro sin licencia</option>
+										<option value="2">Servicio de Notario</option>
+										<option value="3">Preparacion de impuestos</option>
+										<option value="4">Aplicacion y renovacion de numeros ITIN</option>
+										<option value="0">Otro</option>
+									</select>
+								</div>
+								<div class="col-12">
+									<label for="message">Mensaje</label>
+									<textarea name="message" id="message" rows="5"></textarea>
+								</div>
+								<div class="col-12">
+									<ul class="actions">
+										<li><input type="reset" value="Limpiar" /></li>
+										<li><input type="submit" value="Enviar" class="primary" /></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+					</section>
+					<section>
+						<h2>Otros medios de Contacto</h2>
+						<ul class="bulleted-icons">
+							<li>
+								<span class="icon-wrapper"><span class="icon solid fa-envelope"></span></span>
+								<h3>Email</h3>
+								<p><a href="mailto:info@mccstaxservice.com">info@mccstaxservice.com</a></p>
+							</li>
+							<li>
+								<span class="icon-wrapper"><span class="icon solid fa-fax"></span></span>
+								<h3>Fax</h3>
+								<p>515809-3627</p>
+							</li>
+							<li>
+								<span class="icon-wrapper"><span class="icon solid fa-phone"></span></span>
+								<h3>Phone</h3>
+								<p>(515) 243-1986 <br /> (515) 630-0046</p>	
+							</li>
+							<li>
+								<span class="icon-wrapper"><span class="icon brands fa-twitter"></span></span>
+								<h3>Twitter</h3>
+								<p><a href="#">twitter.com/untitled-tld</a></p>
+							</li>
+							<li>
+								<span class="icon-wrapper"><span class="icon solid fa-home"></span></span>
+								<h3>Mailing Address</h3>
+								<p>4200 SE 14TH ST<br />
+								DES MOINES, IA 50320<br />
+								United States
+								</p>
+							</li>
+							<li>
+								<span class="icon-wrapper"><span class="icon brands fa-facebook-f"></span></span>
+								<h3>Facebook</h3>
+								<p><a href="#">facebook.com/mccstaxservice</a></p>
+							</li>
+							
+						</ul>
+					</section>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<p class="copyright">&copy; MCCS Tax Service. All rights reserved.</p>
+					<ul class="menu">
+						<li><a href="#">Terms<span> of Use</span></a></li>
+						<li><a href="#">Privacy<span> Policy</span></a></li>
+						<li><a href="#">Legal<span> Information</span></a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
