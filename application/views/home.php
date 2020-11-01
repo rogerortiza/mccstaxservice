970aad2d798ecf93d17cc354bf6e59e0962edcde
rortiz <rortiz@contractroom.com>
2020-11-01 00:38:09 -0600

    adding more info about services

--- application/views/home.php
+++ application/views/home.php
@@ -11,31 +11,21 @@
 
 		<!-- Header -->
 			<header id="header" class="alt">
-				<!-- <h1><a href="index.html">MCCS <span>Tax Service</span></a></h1> -->
-				<!-- <nav id="nav">
+				<h1><a href="index.html">MCCS <span>Tax Service</span></a></h1>
+				<nav id="nav">
 					<ul>
 						<li><a href="index.html">Home</a></li>
 						<li>
-							<a href="#" class="submenu fa-angle-down">Page Layouts</a>
+							<a href="#" class="submenu fa-angle-down">Servicios</a>
 							<ul>
-								<li><a href="left-sidebar.html">Left Sidebar</a></li>
-								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="no-sidebar.html">No Sidebar</a></li>
-								<li>
-									<a href="#">Submenu</a>
-									<ul>
-										<li><a href="#">Option One</a></li>
-										<li><a href="#">Option Two</a></li>
-										<li><a href="#">Option Three</a></li>
-										<li><a href="#">Option Four</a></li>
-									</ul>
-								</li>
+							<li><a href="left-sidebar.html">Declaracion de Impuestos</a></li>
+								<li><a href="right-sidebar.html">Seguro de Auto sin licencia</a></li>
+								<li><a href="no-sidebar.html">Aplicacion numero ITIN</a></li>
+                                <li><a href="no-sidebar.html">Servicio de Notario</a></li>
 							</ul>
 						</li>
-						<li><a href="elements.html">Elements</a></li>
-						<li><a href="#" class="button">Sign Up</a></li>
 					</ul>
-				</nav>-->
+				</nav>
 			</header>
 
 		<!-- Banner -->
@@ -49,13 +39,13 @@
 				<article>
 					<img src="assets/images/tax-man2.jpg" alt="" />
 					<div class="inner">
-						<h2>MCCS Tax Service and Insurance</h2>
+						<h2>Nos renovamos para ti</h2>
 					</div>
 				</article>
 				<article>
 					<img src="assets/images/tax2.jpg"  alt="" />
 					<div class="inner">
-						<h2>MCCS Tax Service and Insurance</h2>
+						<h2>Para ofrecerte una mejor calidad de servicio</h2>
 					</div>
 				</article>
 			</section>
@@ -65,10 +55,14 @@
 				<div class="inner">
 					<section class="spotlight">
 						<div class="content">
-							<h2>Seguros de carro sin licencia</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id interdum magna, ultricies aliquet curabitur sed metus pellentesque, ornare sapien quis.</p>
+							<h2>Nueva imagen, mejor calidad de servicio pero los mismos de siempre</h2>
+							<p>Cesar Tax es ahora <strong>MCCS Tax and Service</strong>. Nos renovamos con mas servicios y hemos
+								mejorado los que ya teniamos. Ahora contamos con pagina web y redes sociales en donde podras hacer citas,
+								informarte sobre nuestros servicios y requisitos necesarios para tus tramite para realizar tus tramites 
+								asi como para ponerte en contacto con nosotros de una manera mas facil y sencilla. 
+							</p>
 						</div>
-						<span class="image"><img src="assets/images/driver.jpg" alt="" /></span>
+						<span class="image"><img src="assets/images/pay-tax.jpg" alt="" /></span>
 					</section>
 				</div>
 			</section>
@@ -78,19 +72,24 @@
 				<div class="inner">
 					<div class="features">
 						<section class="content">
-							<span class="icon-wrapper major"><span class="icon solid fa-pencil-alt"></span></span>
-							<h3>Servicio de Notario</h3>
-							<p>Proin consequat luctus elit, nec blandit tellus ut volutpat magna. mi euismod elementum lectus et consequat etiam lorem etiam sed tempus. Feugiat veroeros lorem ipsum dolor.</p>
+							<span class="icon-wrapper major"><span class="icon solid fa-award"></span></span>
+							<h3>Experiencia</h3>
+							<p>Con  más de 15 años de experiencia te podemos ayudar con la preparación de tus impuestos personales formulario 
+								1040 o si eres un subcontratista y recibes un formulario 1099 misc por tu trabajo realizado por el año.</p>
 						</section>
 						<section class="content">
-							<span class="icon-wrapper major"><span class="icon solid fa-paper-plane"></span></span>
-							<h3>Preparacion de impuestos</h3>
-							<p>Pellentesque, ornare sapien quis, tristique ante. Proin nec facilisis odio. Integer elementum nunc nec leo interdum, non tristique eros laoreet. Integer vitae erat suscipit commodo.</p>
+							<span class="icon-wrapper major"><span class="icon solid fa-file-contract"></span></span>
+							<h3>Calidad de Servicio</h3>
+							<p>Realizamos tu declaración de impuestos de este año o años anteriores, somos un proveedor autorizado por el IRS 
+								Contamos con E-file para el envío de tu declaración de impuestos</p>
 						</section>
 						<section class="content">
-							<span class="icon-wrapper major"><span class="icon solid fa-list-ol"></span></span>
-							<h3>Aplicacion y renovacion de numeros ITIN</h3>
-							<p>Duis vulputate sit amet metus quis facilisis. Sed dapibus neque erat fringilla tincidunt. Nullam sapien et sapien, iaculis ac varius ultrices nec metus. Aenean ultricies magna.</p>
+							<span class="icon-wrapper major"><span class="icon solid fa-medal"></span></span>
+							<h3>Certificados</h3>
+							<p>Cada uno de tus preparadores está registrado y autorizado por el IRS para la preparación de tu declaración.
+							Si tuviste problemas o quieres hacer modificaciones de declaraciones anteriores te podemos ayudar con la 
+							preparación de tu formulario 1040x Amended
+							</p>
 						</section>
 					</div>
 				</div>
@@ -99,12 +98,63 @@
 		<!-- Three -->
 			<section id="three" class="wrapper style2">
 				<div class="inner">
+					<h2>Servicios</h2>
+					<div class="posts">
+						<section class="post post2">
+							<span class="image"><img src="assets/images/income-small.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Declaracion de Impuestos</h3>
+								<p>Si es la primera vez que nos vistas recuerda traer la siguiente documentación para poder realizar de forma más 
+									eficiente tu declaración de impuestos.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">Saber mas...</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="post post2">
+							<span class="image"><img src="assets/images/driver.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Seguros de auto sin licencia.</h3>
+								<p>Ultrices nec metus. Aenean ultricies magna et sapien rhoncus ac ullamcorper lorem convallis. Quisque at venenatis nisi amet finibus mauris. Sed sodales ultricies magna etiam.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">More</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="post post2">
+							<span class="image"><img src="assets/images/itin.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Aplicacion y renovacion de numeros <strong>ITIN</strong></h3>
+								<p>Si vas a realizar el tramite tu numero de ITIN por primera vez los requisitos son los siguiente.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">Saber mas...</a></li>
+								</ul>
+							</div>
+						</section>
+						<section class="post post2">
+							<span class="image"><img src="assets/images/notary.jpg" alt="" /></span>
+							<div class="content">
+								<h3>Servicio de Notario</h3>
+								<p>Ultrices nec metus. Aenean ultricies magna et sapien rhoncus ac ullamcorper lorem convallis. Quisque at venenatis nisi amet finibus mauris. Sed sodales ultricies magna etiam.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">More</a></li>
+								</ul>
+							</div>
+						</section>
+						
+					</div>
+				</div>
+			</section>
+
+		<!-- Four -->
+		<section id="four" class="wrapper style1">
+				<div class="inner">
 					<h2>Nuestro Team</h2>
 					<div class="posts">
 						<section class="post">
 							<span class="image"><img src="assets/images/pic01.jpg" alt="" /></span>
 							<div class="content">
-								<h3>Pablo Ruano</h3>
+								<h3>Pablo</h3>
 								<p>Aenean ultricies magna non sapien rhoncus, ac ullamcorper lorem convallis. Quisque at venenatis nisi, amet finibus mauris. Sed sodales ultricies eros, sit amet sodales sapien.</p>
 								<ul class="actions">
 									<li><a href="#" class="button">More</a></li>
