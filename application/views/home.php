40938326933ee0dd2305f1dc0b0637c0acf31211
rortiz <rortiz@contractroom.com>
2020-12-02 12:45:31 -0600

    adding messenger chat

--- application/views/home.php
+++ application/views/home.php
@@ -8,6 +8,33 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
+		<!-- Facebook messenger -->
+		<!-- Load Facebook SDK for JavaScript -->
+		<div id="fb-root"></div>
+		<script>
+			window.fbAsyncInit = function() {
+			FB.init({
+				xfbml            : true,
+				version          : 'v9.0'
+			});
+			};
+
+			(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+		<!-- Your Chat Plugin code -->
+		<div class="fb-customerchat"
+			attribution=setup_tool
+			page_id="103601571595772"
+			theme_color="#0A7CFF"
+			logged_in_greeting="Hola! Como te podemos apoyar el dia de hoy?"
+			logged_out_greeting="Hola! Como te podemos apoyar el dia de hoy?">
+		</div>
 
 		<!-- Header -->
 			<header id="header" class="alt">
